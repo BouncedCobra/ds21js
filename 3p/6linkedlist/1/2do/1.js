7640043dd0e321b1f240008abb3d828b903ce39b
BouncedCobra <strujillo1@ucol.mx>
2021-10-28 10:45:19 -0500

    28/10/2021
    28/10/2021

--- 3p/6linkedlist/1/2do/1.js
+++ 3p/6linkedlist/1/2do/1.js
@@ -20,19 +20,92 @@
  * @see {@link https://jsdoc.app}
  */
  
- var d;
- var v = "25";//nuevo elemento
- var array= new Array();
+ let l = new LinkedList
+ l.InsertFirst(25)
+ l.InsertEnd(45)
+ l.InsertEnd(28)
+ l.InsertEnd(17)
+ l.InsertEnd(16)
+ l.InsertEnd(15)
+ l.InsertEnd(14)
+ l.InsertEnd(13)
+
  
- for(i=0; i<25; i++){
-     array.push((Math.random()*25).toFixed());
- }
- console.log(array);
- 
- contains(v);
-function contains(v){
-    print(array.includes(v));
-}
-function print(d){
-    console.log(d);
+class Node {     
+    constructor(data,next=null){         
+        this.data=data         
+        this.next=next     
+    } 
+} 
+
+class LinkedList{     
+    constructor(){         
+        this.head=null         
+        this.size=0 
+        this.tail=null    
+    } 
+
+    AddFirst(data){
+        let node=new  Node(data, null)     
+        this.head=node
+        if(this.tail==null){
+            this.tail=node
+        }
+        this.size++     
+    }
+
+     AddEnd(data){
+        let node=new Node(data,null)
+        this.tail.next=node
+        this.tail=node
+        this.size++
+    } 
+
+    PrintList(){         
+        var current=this.head 
+
+        while(current){         
+            console.log(current.data)         
+            current=current.next         
+        }     
+    }  
+    
+    Contains(v){
+        var current=this.head 
+        var contains=false
+
+        while(current){
+            if(current.data==v){
+                contains=true
+            }                    
+            current=current.next         
+        } 
+        console.log(contains)
+    }
+    
+    AddAt(data,index){         
+        if(index>0 && index>this.size){             
+            console.log("Fuera de Rango")         
+        }         
+        if(index===0){             
+            this.InsertFirst(data)             
+            return         
+        }         
+        var node=new Node(data)         
+        var current,previous         
+        current=this.head         
+        var count=0         
+        while(count<index){             
+            previous=current             
+            count++             
+            current=current.next             
+        }         
+        node.next=current         
+        previous.next=node         
+        this.size++          
+    }
+    
+    SizeList(){
+            console.log(this.size)
+    }
 }
